d95a9674d9d83690d0b30f279aec6a11aaef9e05
hungnd@youmed.vn <hungnd@youmed.vn>
2023-06-14 16:45:54 +0700

    DSA-04: remove sout

--- src/main/java/Part4.java
+++ src/main/java/Part4.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 
 @Log4j2
 public class Part4 {
+    /*
+    * Nhập vào một số nguyên dương n, và n số nguyên lần lượt là các phần tử trong dãy a. Tiếp theo nhập vào hai số nguyên là k và x (0 ≤ k ≤ n).
+    * Hãy chèn phần tử có giá trị x vào dãy a, ở trước phần tử có chỉ số k và sau phần tử có chỉ số k-1.
+    * In mảng kết quả ra màn hình, mỗi số cách nhau bởi một khoảng trắng.
+    * */
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("nhap length: ");
@@ -23,7 +29,7 @@ public class Part4 {
             array[i] = array[i - 1];
         }
         array[k] = x;
-        String arrayToString = null;
+        String arrayToString = "";
         for(int i : array){
             arrayToString = arrayToString.concat(String.valueOf(i)).concat(" ");
         }
